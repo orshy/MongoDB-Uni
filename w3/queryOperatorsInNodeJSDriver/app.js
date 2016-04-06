7f26743f86d0287601be3c30e56aaf1f440fcc87
Philip Orchard <philiporchard@gmail.com>
2016-04-05 17:12:50 -0700

    in the NodeJS Driver

--- w3/queryOperatorsInNodeJSDriver/app.js
+++ w3/queryOperatorsInNodeJSDriver/app.js
@@ -1,5 +1,5 @@
 var MongoClient = require('mongodb').MongoClient,
-    commandLineArgs = require('command-line-args'), 
+    commandLineArgs = require('command-line-args'),
     assert = require('assert');
 
 
@@ -9,9 +9,9 @@ MongoClient.connect('mongodb://localhost:27017/crunchbase', function(err, db) {
 
     assert.equal(err, null);
     console.log("Successfully connected to MongoDB.");
-    
+
     var query = queryDocument(options);
-    var projection = {"_id": 1, "name": 1, "founded_year": 1,
+    var projection = {"_id": 0, "name": 1, "founded_year": 1,
                       "number_of_employees": 1, "crunchbase_url": 1};
 
     var cursor = db.collection('companies').find(query, projection);
@@ -36,20 +36,21 @@ MongoClient.connect('mongodb://localhost:27017/crunchbase', function(err, db) {
 function queryDocument(options) {
 
     console.log(options);
-    
+
     var query = {
         "founded_year": {
             "$gte": options.firstYear,
             "$lte": options.lastYear
-        }
+        },
+        // "number_of_employees": options.employees
     };
 
     if ("employees" in options) {
         query.number_of_employees = { "$gte": options.employees };
     }
-        
+
     return query;
-    
+
 }
 
 
@@ -60,7 +61,7 @@ function commandLineOptions() {
         { name: "lastYear", alias: "l", type: Number },
         { name: "employees", alias: "e", type: Number }
     ]);
-    
+
     var options = cli.parse()
     if ( !(("firstYear" in options) && ("lastYear" in options))) {
         console.log(cli.getUsage({
@@ -71,7 +72,5 @@ function commandLineOptions() {
     }
 
     return options;
-    
+
 }
-
-
