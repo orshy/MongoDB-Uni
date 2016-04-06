b8f4dbc06f57f3c5b15595c8a898a6d2c40d06e1
Philip Orchard <philiporchard@gmail.com>
2016-04-05 18:48:51 -0700

    npm

--- w3/dotNotationInNodeJSDriver/app.js
+++ w3/dotNotationInNodeJSDriver/app.js
@@ -1,5 +1,5 @@
 var MongoClient = require('mongodb').MongoClient,
-    commandLineArgs = require('command-line-args'), 
+    commandLineArgs = require('command-line-args'),
     assert = require('assert');
 
 
@@ -10,7 +10,7 @@ MongoClient.connect('mongodb://localhost:27017/crunchbase', function(err, db) {
 
     assert.equal(err, null);
     console.log("Successfully connected to MongoDB.");
-    
+
     var query = queryDocument(options);
     var projection = {"_id": 0, "name": 1, "founded_year": 1,
                       "number_of_employees": 1, "ipo.valuation_amount": 1};
@@ -37,28 +37,27 @@ MongoClient.connect('mongodb://localhost:27017/crunchbase', function(err, db) {
 function queryDocument(options) {
 
     console.log(options);
-    
+
     var query = {
         "founded_year": {
             "$gte": options.firstYear,
             "$lte": options.lastYear
-        }
-    };
+        }};
 
     if ("employees" in options) {
         query.number_of_employees = { "$gte": options.employees };
     }
-    
+
     if ("ipo" in options) {
         if (options.ipo == "yes") {
             query["ipo.valuation_amount"] = {"$exists": true, "$ne": null};
         } else if (options.ipo == "no") {
             query["ipo.valuation_amount"] = null;
-        }               
+        }
     }
-    
+
     return query;
-    
+
 }
 
 
@@ -70,7 +69,7 @@ function commandLineOptions() {
         { name: "employees", alias: "e", type: Number },
         { name: "ipo", alias: "i", type: String }
     ]);
-    
+
     var options = cli.parse()
     if ( !(("firstYear" in options) && ("lastYear" in options))) {
         console.log(cli.getUsage({
@@ -81,7 +80,5 @@ function commandLineOptions() {
     }
 
     return options;
-    
+
 }
-
-
